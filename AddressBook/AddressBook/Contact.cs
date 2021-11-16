89b482ce0de2544d7132efb6afe11e7c181f49f0
suraj9982 <surajsk9982@gmail.com>
2021-11-16 13:16:07 +0530

    [suraj] add : creating contact in specific order

---
+++ AddressBook/AddressBook/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Contact
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string Address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string phone_number { get; set; }
+        public string email_id { get; set; }
+    }
+}
